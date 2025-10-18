34a843ac9995acd2f161b4aee9ed7a05c526d86e
Andrei Dina <andreidina04@gmail.com>
2025-10-18 23:45:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function joc() {
 let lightOn = false;
 function light() {
     if(!lightOn) {
-    document.body.style.backgroundColor = "gray";
+    document.body.style.backgroundColor = "#818589";
     document.body.style.color = "black";
     lightOn = true;
     }
